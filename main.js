c40746339b07128771f2389eb9667b52ebc6961f
LokankshiSharma <81632228+LokankshiSharma@users.noreply.github.com>
2023-08-14 15:33:46 -0400

    Update main.js

--- main.js
+++ main.js
@@ -14,31 +14,31 @@ function draw(){
     image(img, 0, 0, 600, 400);
 
     fill("black");
-    text ("Kazuha", 90, 175);
+    text ("Kazuha", 100, 175);
     noFill();
     stroke ("white");
     rect(20, 10, 150, 350);
 
     fill("pink");
-    text ("Chaewon", 220, 175);
+    text ("Chaewon", 175, 175);
     noFill();
     stroke ("pink");
     rect(170, 10, 150, 350 );
 
     fill("yellow");
-    text ("Sakura", 340, 175);
+    text ("Sakura", 250, 175);
     noFill();
     stroke ("yellow");
     rect(320, 10, 150 , 350);
 
     fill("purple");
-    text ("Eunchae", 500, 175);
+    text ("Eunchae", 325, 175);
     noFill();
     stroke ("purple");
     rect(470, 10, 120 , 350);
 
     fill("blue");
-    text ("Yunjin", 600, 175);
+    text ("Yunjin", 400, 175);
     noFill();
     stroke ("purple");
     rect(470, 10, 120 , 350);
