7d21ad45b3da28d4a41e2cb28a7f36ffbd9cbdc1
LokankshiSharma <81632228+LokankshiSharma@users.noreply.github.com>
2023-08-14 15:11:39 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+var img = "";
+
+function setup(){
+    canvas= createCanvas(800, 600);
+    canvas.position(360, 190); 
+
+}
+
+function preload(){
+    img = loadImage("ls.png");
+}
+
+function draw(){
+    image(img, 0, 0, 800, 600);
+
+    fill("black");
+    text ("Kazuha", 90, 175);
+    noFill();
+    stroke ("white");
+    rect(20, 10, 150, 350);
+
+    fill("pink");
+    text ("Chaewon", 220, 175);
+    noFill();
+    stroke ("pink");
+    rect(170, 10, 150, 350 );
+
+    fill("yellow");
+    text ("Sakura", 340, 175);
+    noFill();
+    stroke ("yellow");
+    rect(320, 10, 150 , 350);
+
+    fill("purple");
+    text ("Jisoo", 500, 175);
+    noFill();
+    stroke ("purple");
+    rect(470, 10, 120 , 350);
+
+    fill("purple");
+    text ("Yunjin", 500, 175);
+    noFill();
+    stroke ("purple");
+    rect(470, 10, 120 , 350);
+}
